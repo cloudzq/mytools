f5e2a5c0b55fec6111de071a33d78fa7c720d042
cloudzq <33003023+cloudzq@users.noreply.github.com>
2018-08-20 17:26:42 +0800

    1
    1

--- mail.py
+++ mail.py
@@ -3,144 +3,106 @@
 @Project:watchmen
 @Language:Python3.6.4
 @Author:Hans
-@File:oracleClient.py
+@File:mail.py
 @Ide:PyCharm
-@Time:2018/8/20 15:44
+@Time:2018/8/20 14:59
 @Remark:
 """
 
-import re
-import datetime
-import cx_Oracle
+import time
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from functools import wraps
+from email.mime.application import MIMEApplication
 
 
-class OracleClient(object):
-    def __init__(self, db_url='username/username@dbname.com.cn:1534/dbname'):
-        self.db_url = db_url
-        self.conn = None
+class MailClient(object):
+    def __init__(self, mail_to_list, mail_cc_list, mail_title, html_file,
+                 mail_user='sboms@xxx.com.cn',
+                 mail_host='192.168.108.193', attachment=[]):
+        """
+        email function
+        :param mail_to_list: 发给谁 ,list or tuple
+        :param mail_cc_list:抄送给谁 ,list or tuple
+        :param mail_title: 邮件标题 ,str
+        :param html_file:邮件正文 ,str
+        :param mail_user:发送邮件的人 ,str
+        :param mail_host: SMTP服务器地址 ,str
+        :param attachment: 附件 ,默认为None,list or tuple 绝对路径
+        """
 
-    def __enter__(self):
+        print(mail_to_list)
+        print(mail_cc_list)
+
+        self.mail_tolist = ['xxx@xxx.com.cn','<xxx@xxx.com.cn>']
+        self.mail_cclist = ['xxx@xxx.com.cn']
+
+        self.mail_title = mail_title
+        self.attachment = attachment
+        self.mail_content = self._get_content(html_file)
+        self.mail_host = mail_host
+        self.mail_user = mail_user
+
+    def _get_content(self,html_file):
         try:
-            self.conn = self._connect()
-            if self.conn:
-                return self.conn
-        except Exception as e:
-            raise Exception("connet database failed...")
+            with open(html_file,'r',encoding='UTF-8') as f:
+                content = f.read()
+                return content
+        except Exception:
+            return html_file
 
-    def __exit__(self, exc_type, exc_val, exc_tb):
-        if self.conn:
-            self.conn.close()
-        self.conn = None
+    def retry_send(num=3,interval=5):
+        def inner(func):
+            @wraps(func)
+            def wrap(*args,**kwargs):
+                count = 0
+                while count < num:
+                    ret = func(*args,**kwargs)
+                    if ret:
+                        return ret
+                    count += 1
+                    time.sleep(interval)
+                return ret
+            return wrap
+        return inner
 
-    def oracle_operation(self, sql):
-        """
-        oracle insert,delete,query,update function
-        :param sql: str
-        :return: None or list
-        """
-        result = None
-        if self.conn is None:
-            self.conn = self._connect()
-        cur = self.conn.cursor()
-        cur.execute(sql)
-        self.conn.commit()
-
-        if 'select' in sql or 'SELECT' in sql:
-            result = cur.fetchall()
-        cur.close()
-        return result
-
-    def close(self):
-
-        """
-        close session
-        :return: None
-        """
-
-        self.conn.close()
-
-    def _connect(self):
-        conn = cx_Oracle.connect(self.db_url)
-        return conn
-
-    def _insert_row(self, sql):
-        if not self.conn:
-            self.conn = self._connect()
+    @retry_send(3,5)
+    def send_mail(self):
+        me = self.mail_user
+        msg = MIMEMultipart
+        msg['Subject'] = self.mail_title
+        msg['From'] = me
+        msg['To'] = ";".join(self.mail_tolist)
+        msg['Cc'] = ";".join(self.mail_cclist)
         try:
-            self.conn.cursor().execute(sql)
-            self.conn.commit()
-        except Exception as e:
-            self.close()
-            raise Exception(e.__str__())
-
-    def insert_database(self, json_file):
-        """
-        insert data by json's file
-
-        :param json_file: str
-        :return: None
-        """
-
-        date = datetime.datetime.now().strftime("%Y-%m-%d")
-        file_dir = json_file
-        file_object = open(file_dir, 'r', 'utf-8')
-        try:
-            raw_data = file_object.read()
-        finally:
-            file_object.close()
-        db_data = eval(raw_data)
-
-        for table in db_data.get('tables'):
-            table_name = table.get('name')
-            for row in table.get('data'):
-                if isinstance(row, dict):
-                    values = ""
-                    fileds = ""
-                    for filed_key, filed_value in row.items():
-                        fileds += "  %s," % (filed_key)
-                        if isinstance(filed_value, str) and re.match('\d{4}-\d{2}-\d{2}', filed_value):
-                            filed_value = "to_date('%s','yyyy-mm-dd hh24:mi:ss')" % (filed_value)
-                            values += "  %s," % (filed_value)
-                        else:
-                            values += "  '%s'," % (filed_value)
-
-                    sql = "INSERT INTO %s (%s) VALUES (%s)" % (table_name, fileds.rstrip(','),
-                                                               values.rstrip(','))
-                    self._insert_row(sql)
-                else:
-                    sql = "insert into " + table_name + "values(to_date('" + date + " ','yyyy-mm-dd'),"
-                    for values in row:
-                        sql += "'" + str(values) + "',"
-                    sql = sql[0:len(sql) - 1] + ")"
-                    self._insert_row(sql)
-        self.close()
-
+            mailtext = MIMEText(self.mail_content,_subtype='html',_charset='utf-8')
+            msg.attach(mailtext)
+            #添加附件
+            for file in self.attachment:
+                with open(file,'rb') as f:
+                    content = f.read()
+                part = MIMEApplication(content)
+                part.add_header('Content-Disposition','attachment',filename=file.split('/')[-1])
+                msg.attach(part)
+            s = smtplib.SMTP()
+            #连接到指定的SMTP服务器，参数分别表示SMTP主机和端口
+            s.connect(self.mail_host)
+            #发送内容
+            s.sendmail(me,self.mail_tolist + self.mail_cclist,msg.as_string())
+            s.close()
+            return True
+        except Exception:
+            return False
 
 if __name__ == '__main__':
-    # 上下文管理方式，返回数据库连接对象conn，适合一次性操作数据库
-    with OracleClient() as conn:
-        # sql = 'select * from mytable where rownum<5'
-        sql = "delete from mytable where name=43332"
-        cur = conn.cursor()
-        cur.execute(sql)
-        # result = cur.fetchall()
-        # for row in result:
-        #     print(row)
-        cur.commit()
+    client = MailClient(mail_to_list=['xxx@xxx.com.cn'],
+               mail_cc_list=['xxx@xxx.com.cn','xxb@xxx.com.cn'],
+               html_file='/tmp/pages.html',
+               attachment=['/tmp/pages.html',
+                           '/tmp/test.sh',
+                           '/home/mypages.tgz'],
+               mail_user='xxx@xxx.com.cn',
+               mail_title='xxxxx')
 
-    # 初始化oracle客户端后多次操作
-    client = OracleClient(db_url='username/username@dbname.com.cn:1534/dbname')
-    sql1 = 'select * from mytable where rownum<5'
-    sql2 = 'update mytable set name=2231 where id =123'
-    result1 = client.oracle_operation(sql1)
-    result2 = client.oracle_operation(sql2)
-    client.close()
-    # 从文件中读取要插入的数据
-    client = OracleClient(db_url='username/username@dbname.com.cn:1534/dbname')
-    client.insert_database('/home/dir/test.txt')
-    #从给定的文件中插入数据
-    client = OracleClient(db_url='username/username@dbname.com.cn:1534/dbname')
-    client.insert_database(json_file)
-    #json_file格式：{"a":"b","c":"d","e":[{"a1":"a2","b1":[[87,7],[76,6]]}]}
-    #{"a":"b","c":"d","e":[{"a1":"a2"},{"b1":"b2"}]}
-    #
+    client.send_mail()
